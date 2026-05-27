4c9b66969c5db54f957ffef414c1f65be9c3a821
gatorolo77 <rodri@northpay.com>
2026-05-27 03:51:08 -0300

    fix: solucionar error de ruteo NG04002

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OnboardingComponent } from './onboarding/onboarding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'onboarding', component: OnboardingComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
